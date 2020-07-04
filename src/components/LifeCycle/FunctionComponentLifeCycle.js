7a1230cd5a13250e2b793563bbe7b99277f33503
LIMJONGSOO <limjs4535@gmail.com>
2020-07-04 23:58:58 +0900

    hook reducer

--- src/components/LifeCycle/FunctionComponentLifeCycle.js
+++ src/components/LifeCycle/FunctionComponentLifeCycle.js
@@ -1,7 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value + 1 };
+    default:
+      return state;
+  }
+}
 
 const FunctionComponentLifeCycle = ({ name, number, children }) => {
   const [stateNumber, setStateNumber] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
   // mount, update 되면 실행
   // 두번째 인자 배열에 있는 값이 업데이트 되면 실행, 빈 배열이면 마운트 일 때만 실행
   useEffect(() => {
@@ -20,7 +33,12 @@ const FunctionComponentLifeCycle = ({ name, number, children }) => {
 
   return (
     <div>
+      <p>현재 State value {{ stateNumber }}</p>
       <button onClick={onClickStateNumber}>+1</button>
+      <p>현재 Reducer value {{ state }}</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>
+        reducer dispatch +1
+      </button>
     </div>
   );
 };
